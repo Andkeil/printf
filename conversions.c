1d9e19cdc4ce5c17c2f014233e03ff51444ef883
Andrew Keil <monkeyninja147@gmail.com>
2018-02-26 00:16:44 +0000

    Fixed conversion for chars

--- conversions.c
+++ conversions.c
@@ -1,8 +1,11 @@
 #include "holberton.h"
 
-int print_d(va_list list, char *buffer, int *ip)
+int print_c(va_list list, char *buffer, int *ip)
 {
-	buffer[0] = 'c';
+	char c;
+
+	c = va_arg(list, int);
+	buffer[*ip] = c;
 	/*when you wrote to buffer, you forgot to increment *ip*/
 	(*ip)++;
 	return(*ip);
