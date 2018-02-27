f0b752da6b94b836a2e1d9ccc6be96fbeb6762ed
maupinpamela <maupinpamela@gmail.com>
2018-02-27 23:11:42 +0000

    Fixed betty errors

--- formatter.c
+++ formatter.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
-
+/**
+ *formatter- main for array of structs
+ * @c: char pointer
+ * Return: returns the proper struct that is called
+ */
 int (*formatter(char c))(va_list, char *, int *)
 {
 	int i = 0;
 	converter_t form[] = {
-	       	{'d', print_d},
-	       	{'b', print_binary},
+		{'d', print_d},
+		{'b', print_binary},
 		{'c', print_c},
-	       	{'i', print_d},
+		{'i', print_d},
 		{'s', print_s},
-		/*	{'u', print_u},
-		{'o', print_o},
-		{'x', print_hex},
-	       	{'X', print_ux},
-		*/{'%', print_percent},
+		{'%', print_percent},
 		{'r', print_rev},
 		{'R', print_rot},
-	       	{'\0', NULL}
+		{'\0', NULL}
 	};
 
 	while (form[i].tp != '\0')
